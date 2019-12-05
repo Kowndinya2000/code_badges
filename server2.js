a6b18c55fe66c7788f77a4ddf1e1b9887ecad9ec
Kowndinya2000 <kowndiboyalakuntla@gmail.com>
2019-12-06 00:05:57 +0530

    second commit

--- server2.js
+++
@@ -1,8 +0,0 @@
-var cmd=require('node-cmd');
- 
-    cmd.get(
-        'node index3.js',
-        function(err, data, stderr){
-            console.log("Working Server is initiated");
-        }
-    );
